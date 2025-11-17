b5b7e0d7a2471b91a78003f92a1ae1f26dfd29f4
Hiseous <hiseous@gmail.com>
2025-11-17 04:27:47 +0100

    debugging env vars

--- src/app/api/home/getFeaturedVendors/route.ts
+++ src/app/api/home/getFeaturedVendors/route.ts
@@ -2,7 +2,7 @@ import { fetchData } from "@/lib/fetcher";
 export async function GET() {
   console.log('----process-env-server', {all: process.env, foc: process.env.BASE_URL, soc: process.env.NEXT_PUBLIC_SOCKET_URL});
   const response = await fetchData(
-    `${process.env.BASE_URL}/stores/featured`,
+    `${process.env.NEXT_PUBLIC_BASE_URL}/stores/featured`,
     "GET"
   );
 
